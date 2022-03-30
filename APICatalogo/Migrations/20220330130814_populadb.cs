e187c07d6fed03ba796733e420b52764a13298c1
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-03-30 11:04:04 -0300

    populating database

---
+++ APICatalogo/Migrations/20220330130814_populadb.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class populadb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categories (Name, ImageUrl) Values('Drinks','http://www.macoratti.net/Imagens/1.jpg')");
+
+            mb.Sql("Insert into Categories (Name, ImageUrl) Values('Snacks','http://www.macoratti.net/Imagens/2.jpg')");
+
+            mb.Sql("Insert into Categories (Name, ImageUrl) Values('Desserts','http://www.macoratti.net/Imagens/3.jpg')");
+
+
+
+            mb.Sql("Insert into Products (Name,Description,Price,ImageUrl,Stock,RegistrationDate,CategoryId)" +
+
+              "Values('Coca-cola Diet', 'Soda 350 ml', 5.45,'http://www.macoratti.net/Imagens/coca.jpg',50, now(),(Select Id from Categories where Name='Drinks'))");
+
+
+
+            mb.Sql("Insert into Products (Name,Description,Price,ImageUrl,Stock,RegistrationDate,CategoryId)" +
+
+                "Values('Tuna Snack','Tuna sandwich with tomato sauce',8.50,'http://www.macoratti.net/Imagens/atum.jpg',10, now(),(Select Id from Categories where Name='Snacks'))");
+
+
+
+            mb.Sql("Insert into Products (Name,Description,Price,ImageUrl,Stock,RegistrationDate,CategoryId)" +
+
+                 "Values('Pudding', 'Condensed milk pudding 100g',6.75,'http://www.macoratti.net/Imagens/pudim.jpg',20, now(), (Select Id from Categories where Name='Desserts'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categories");
+
+            mb.Sql("Delete from Products");
+        }
+    }
+}
