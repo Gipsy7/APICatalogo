1ad741bf07310a6c397ee075cfbfb5459312246c
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-03-30 10:01:52 -0300

    Creating migration

--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Categories")]
     public class Category
     {
         public Category()
         {
             Products = new Collection<Product>();
         }
+        [Key]
         public int Id { get; set; } 
+        [Required]
+        [MaxLength(80)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string ImageUrl { get; set; }
         public ICollection<Product> Products { get; set; }
     }
--- APICatalogo/Models/Product.cs
+++ APICatalogo/Models/Product.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Products")]
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
+        [MaxLength(500)]
         public string ImageUrl { get; set; }
         public float Stock { get; set; }
         public DateTime RegistrationDate { get; set; }
